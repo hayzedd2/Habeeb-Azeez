f086a79772ac88017f07cdf703f0b1759f821eb6
hayzedd2 <azeezalhameen1@gmail.com>
2023-11-11 17:45:52 +0100

    mobile dev

--- src/components/HaloWebsite.jsx
+++ src/components/HaloWebsite.jsx
@@ -20,14 +20,14 @@ const HaloWebsite = ({ scrollPosition }) => {
     <>
       <Navbar />
       <section className="min-h-screen w-[100vw] bg-[#111315] flex">
-        <div className="w-1/5">
+        <div className="xl:w-1/5 sm:hidden xl:block">
           <SideBar />
         </div>
-        <div className="w-4/5 max-h-screen overflow-y-auto overflow-x-hidden">
+        <div className="xl:w-4/5 sm:w-full max-h-screen overflow-y-auto overflow-x-hidden">
           <div className="pt-[5rem]">
             <div className="   text-white ">
               <div
-                className="flex gap-5 items-center px-7 pb-5 pt-7 fixed bg-[#111315]  z-30  w-4/5"
+                className="flex gap-5 items-center px-7 pb-5 pt-7 fixed bg-[#111315]  z-30  sm:w-full xl:w-4/5"
                 style={{ borderBottom: "1.5px solid #2c2e30" }}
               >
                 <Link to={"/case-study"}>
@@ -35,13 +35,13 @@ const HaloWebsite = ({ scrollPosition }) => {
                     <FaArrowLeft className="text-[1rem] font-[400] text-black" />
                   </div>
                 </Link>
-                <h1 className=" font-kumbh font-[600] text-[1.05rem] text-grey">
+                <h1 className=" font-kumbh font-[600] xl:text-[1.05rem] sm:text-[0.95rem] text-grey">
                   Halo Website Redesign Case Study
                 </h1>
               </div>
               <div className="px-7">
                 <div className="pt-[7rem]">
-                  <p className="  font-kumbh text-[1.25rem] font-[500]">
+                  <p className="  font-kumbh xl:text-[1.25rem] sm:text-[1rem] xl:leading-normal sm:leading-loose font-[500]">
                     Powering prosperity through Communities helping to provide
                     the best savings and investments to your members.
                   </p>
@@ -260,7 +260,7 @@ const HaloWebsite = ({ scrollPosition }) => {
                           </div>
                         </div>
                       </div>
-                      <div className="colors flex flex-wrap items-center justify-between  mb-16 mt-8">
+                      <div className="colors flex flex-wrap items-center xl:justify-between sm:justify-center xl:gap-0 sm:gap-4  mb-16 mt-8">
                         <div className="color-div bg-[#143284]">
                           <p>#143284</p>
                         </div>
